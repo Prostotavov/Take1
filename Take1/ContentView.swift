810e7fa7314e012e9a9300642b837223e9541e8b
Roma senkevuch <RomaSenkevich@bk.ru>
2021-05-28 06:45:21 +0300

    blur view completed. delete useless checks

--- Take1/ContentView.swift
+++ Take1/ContentView.swift
@@ -89,12 +89,13 @@ struct LibraryView: View {
             if showAddDictView {
                 AddDictView(showAddDictView: $showAddDictView, libr: $libr)
             }
-        }
+        }.ignoresSafeArea()
     }
 }
 
 struct DictionaryView: View {
     
+    @Environment(\.presentationMode) var presentationMode
     @Binding var dict: Dictionary
     @State var showAddWordView = false
     @State var showEditWordView = false
@@ -102,53 +103,63 @@ struct DictionaryView: View {
     
     var body: some View {
         ZStack {
-            List {
-                ForEach(dict.words){ wor in
-                    
-                    Button(action:{
-                        if !showAddWordView && !showEditWordView {
-                        showEditWordView = true
-                        } else {
-                            showEditWordView = false
-                        }
-                        index = dict.words.firstIndex(where: {
-                            $0.id == wor.id
-                        }) ?? 0
-                    }) {
-                        HStack(alignment: .top) {
-                            VStack(alignment: .leading) {
-                                Text(wor.name).font(.headline)
-                                Text(wor.translate)
+            NavigationView {
+                List {
+                    ForEach(dict.words){ wor in
+                        
+                        Button(action:{
+                            showEditWordView = true
+                            index = dict.words.firstIndex(where: {
+                                $0.id == wor.id
+                            }) ?? 0
+                        }) {
+                            HStack(alignment: .top) {
+                                VStack(alignment: .leading) {
+                                    Text(wor.name).font(.headline)
+                                    Text(wor.translate)
+                                }
+                                Spacer()
+                                Text(wor.shortAnalogy)
                             }
-                            Spacer()
-                            Text(wor.shortAnalogy)
+                        }
+                    }
+                    .onDelete { (IndexSet) in
+                        self.dict.words.remove(atOffsets: IndexSet)
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Text(dict.name)
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                                showAddWordView = true
+                        }) {
+                            Image(systemName: "plus.circle.fill")
+                                .font(.title)
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                        }) {
+                            HStack{
+                                Image(systemName: "chevron.left")
+                                    .font(.custom("", fixedSize: 24))
+                                    .offset(x: 5)
+                                Text("Back")
+                            }
+                            .offset(x: -11)
                         }
                     }
                 }
-                .onDelete { (IndexSet) in
-                    self.dict.words.remove(atOffsets: IndexSet)
-                }
             }
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Text(dict.name)
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        if !showEditWordView {
-                        showAddWordView = true
-                        }
-                    }) {
-                        Image(systemName: "plus.circle.fill")
-                            .font(.title)
-                    }
-                }
-            }
-            
-            if showAddWordView && !showEditWordView {
+            .blur(radius: showAddWordView || showEditWordView ? 3.0 : 0)
+            .navigationBarHidden(true)
+            if showAddWordView {
                 AddWordView(showAddWordView: $showAddWordView, dict: $dict)
             }
-            if showEditWordView && !showAddWordView {
+            if showEditWordView {
                 EditWordView(showEditWordView: $showEditWordView, dict: $dict, index: index)
             }
         }
@@ -157,6 +168,7 @@ struct DictionaryView: View {
 
 struct Layer1View_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+         ContentView()
+       // DictionaryView(dict: .constant(Dictionary(name: "")))
     }
 }
